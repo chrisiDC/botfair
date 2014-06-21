f72cb9850f74f332ce8ff684c6dde2f90c9cd17b
chrisiDC <chrisiDC@gmail.com>
2014-06-21 13:03:13 +0200

    v1.0

---
+++ Bot.Shared/IBotServer.cs
@@ -0,0 +1,27 @@
+﻿using System.Data;
+
+namespace Bot.Shared
+{
+    public enum BotState
+    {
+        STARTED,
+        STOPPED,
+        STOPPING,
+        STARTING
+    };
+
+    public interface IBotServer
+    {
+        void ShutDown();
+
+        void Start();
+
+        int GetState();
+
+        bool ChangeConfiguration(int configurationId);
+
+        int GetActiveConfiguration();
+
+        DataTable GetCacheContent(string name,string orderBy);
+    }
+}
---
+++ Bot.Shared/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Bot.Shared")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Bot.Shared")]
+[assembly: AssemblyCopyright("Copyright ©  2012")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("7123ea5c-17cc-4979-ae09-7feeaddbbff5")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
